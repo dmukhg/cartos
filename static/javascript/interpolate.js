4084981eda7185d489b8e0336d8c96f9fac373ff
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-07 16:23:35 +0530

    Add colorpicker, and interpolate between chosen colors

--- static/javascript/interpolate.js
+++ static/javascript/interpolate.js
@@ -1,4 +1,4 @@
-/* Interpolate between colours. */
+
 define([], function () {
   var colours = {
     darkGrey  : [73, 105, 122],
@@ -16,11 +16,26 @@ define([], function () {
   , interpolate = function() {
     var c = colours; // local reference to save lookups
 
-    if (arguments.length = 1) {
-       // Use the only argument (between 0 and 1) to return a grayscale colour.
-       var r = arguments[0] * ( c.lightGrey[0] - c.darkGrey[0] ) + c.darkGrey[0]
-         , g = arguments[0] * ( c.lightGrey[1] - c.darkGrey[1] ) + c.darkGrey[1]
-         , b = arguments[0] * ( c.lightGrey[2] - c.darkGrey[2] ) + c.darkGrey[2];
+    if (arguments.length == 1) {
+      // Use the only argument (between 0 and 1) to return a grayscale colour.
+      var c1 = c.lightGrey,
+          c2 = c.darkGrey; 
+
+      var r = arguments[0] * ( c1[0] - c2[0] ) + c2[0]
+        , g = arguments[0] * ( c1[1] - c2[1] ) + c2[1]
+        , b = arguments[0] * ( c1[2] - c2[2] ) + c2[1];
+
+      return rgbToString(r,g,b);
+    }
+
+    if (arguments.length == 3) {
+      // Use the specified colours to interpolate
+      var c2 = arguments[1],
+          c1 = arguments[2];
+      
+      var r = arguments[0] * ( c1[0] - c2[0] ) + c2[0]
+        , g = arguments[0] * ( c1[1] - c2[1] ) + c2[1]
+        , b = arguments[0] * ( c1[2] - c2[2] ) + c2[1];
 
       return rgbToString(r,g,b);
     }
