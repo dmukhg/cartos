f050b3f441649cf889c99ca200df8aa2f0834197
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-22 14:11:09 +0530

    map: add tests for multiple model attributes

--- spec/models.spec.js
+++ spec/models.spec.js
@@ -1,16 +1,20 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
 define(["../javascript/models/map.js"], function (Map) {
-  var m = new Map({'startColor': '#ff9999'});
+  var m = new Map({'startColor': '#ff9999', 'endColor': '#558888'});
 
   describe("Map", function () {
     it("should respect constructor arguments", function () {
       expect('#ff9999').toEqual(m.get('startColor'));
+      expect('#558888').toEqual(m.get('endColor'));
     });
 
     it("should update on calling set", function () {
       m.set('startColor', '#898989');
       expect('#898989').toEqual(m.get('startColor'));
+
+      m.set('endColor', '#454545');
+      expect('#454545').toEqual(m.get('endColor'));
     });
   });
 });
